1111fa90e9784fc63cb0b2ec66073e9529e04807
Nikhitapanigrahi <111998044+Nikhitapanigrahi@users.noreply.github.com>
2024-07-12 16:59:20 +0530

    Test code is Written

--- com.user/src/test/java/Amazon/com/user/AppTest.java
+++ com.user/src/test/java/Amazon/com/user/AppTest.java
@@ -1,8 +1,22 @@
 package Amazon.com.user;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.Test;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+	@Test
+	public int sum(int a,int b) {
+		int expected = 100;
+		int res = a+b;
+		return res;
+	}
+	public static void main(String[] args) {
+		AppTest at = new AppTest();
+		at.sum(50,50);
+	}
 }
